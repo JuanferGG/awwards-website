c307d737c1a7408bbcbe8660ec20649c17a6fad1
JuanferGG <juanfe15777@gmail.com>
2025-10-17 10:49:20 -0500

    feat: NavBar component with GSAP animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import About from "./components/About";
 import { Hero } from "./components/Hero";
+import NavBar from "./components/NavBar";
 
 
 function App() {
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
+    <main className="relative min-h-screen w-screen overflow-x-hidden bg-zinc-300">
+      <NavBar />
       <Hero />
       <About />
     </main>
